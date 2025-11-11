36787f3e9d648ffa0b8545150badea6b4285419a
fa0635 <qwunind5@gmail.com>
2025-11-11 12:00:15 +0300

    Add files via upload

---
+++ libtorchtest_4/build/CMakeFiles/main.dir/link.d
@@ -0,0 +1,241 @@
+main: \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/crt1.o \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/crti.o \
+  /usr/lib/gcc/x86_64-redhat-linux/15/crtbegin.o \
+  CMakeFiles/main.dir/main.cpp.o \
+  /home/andrej/libtorch/lib/libtorch.so \
+  /home/andrej/libtorch/lib/libc10.so \
+  /home/andrej/libtorch/lib/libkineto.a \
+  /home/andrej/miniconda3/lib/libyaml-cpp.so.0.8.0 \
+  /home/andrej/root_install/lib/libCore.so \
+  /home/andrej/root_install/lib/libImt.so \
+  /home/andrej/root_install/lib/libRIO.so \
+  /home/andrej/root_install/lib/libNet.so \
+  /home/andrej/root_install/lib/libHist.so \
+  /home/andrej/root_install/lib/libGraf.so \
+  /home/andrej/root_install/lib/libGraf3d.so \
+  /home/andrej/root_install/lib/libGpad.so \
+  /home/andrej/root_install/lib/libROOTDataFrame.so \
+  /home/andrej/root_install/lib/libTree.so \
+  /home/andrej/root_install/lib/libTreePlayer.so \
+  /home/andrej/root_install/lib/libRint.so \
+  /home/andrej/root_install/lib/libPostscript.so \
+  /home/andrej/root_install/lib/libMatrix.so \
+  /home/andrej/root_install/lib/libPhysics.so \
+  /home/andrej/root_install/lib/libMathCore.so \
+  /home/andrej/root_install/lib/libThread.so \
+  /home/andrej/root_install/lib/libMultiProc.so \
+  /home/andrej/root_install/lib/libROOTVecOps.so \
+  /home/andrej/libtorch/lib/libtorch_cpu.so \
+  /home/andrej/libtorch/lib/libc10.so \
+  /home/andrej/libtorch/lib/libtorch.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libstdc++.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libm.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libm.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libm.so \
+  /lib64/libm.so.6 \
+  /lib64/libmvec.so.1 \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so \
+  /lib64/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libpthread.a \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libc.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libc.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libc.so \
+  /lib64/libc.so.6 \
+  /usr/lib64/libc_nonshared.a \
+  /lib64/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so \
+  /lib64/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a \
+  /usr/lib/gcc/x86_64-redhat-linux/15/crtend.o \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/crtn.o \
+  /usr/lib64/libdl.so.2 \
+  /usr/lib64/libpthread.so.0 \
+  /lib64/ld-linux-x86-64.so.2 \
+  /home/andrej/miniconda3/lib/libpcre2-8.so.0 \
+  /home/andrej/miniconda3/lib/libz.so.1 \
+  /home/andrej/miniconda3/lib/liblzma.so.5 \
+  /home/andrej/miniconda3/lib/libzstd.so.1 \
+  /home/andrej/root_install/lib/libtbb.so.12 \
+  /home/andrej/miniconda3/lib/libssl.so.3 \
+  /home/andrej/miniconda3/lib/libcrypto.so.3 \
+  /usr/lib64/libfreetype.so.6 \
+  /home/andrej/root_install/lib/libROOTNTuple.so \
+  /home/andrej/root_install/lib/libvdt.so \
+  /home/andrej/root_install/lib/libGenVector.so \
+  /usr/lib64/librt.so.1 \
+  /home/andrej/libtorch/lib/libgomp-98b21ff3.so.1 \
+  /usr/lib64/libbz2.so.1 \
+  /usr/lib64/libpng16.so.16 \
+  /usr/lib64/libharfbuzz.so.0 \
+  /usr/lib64/libbrotlidec.so.1 \
+  /usr/lib64/libglib-2.0.so.0 \
+  /usr/lib64/libgraphite2.so.3 \
+  /usr/lib64/libbrotlicommon.so.1
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/crt1.o:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/crti.o:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/crtbegin.o:
+
+CMakeFiles/main.dir/main.cpp.o:
+
+/home/andrej/libtorch/lib/libtorch.so:
+
+/home/andrej/libtorch/lib/libc10.so:
+
+/home/andrej/libtorch/lib/libkineto.a:
+
+/home/andrej/miniconda3/lib/libyaml-cpp.so.0.8.0:
+
+/home/andrej/root_install/lib/libCore.so:
+
+/home/andrej/root_install/lib/libImt.so:
+
+/home/andrej/root_install/lib/libRIO.so:
+
+/home/andrej/root_install/lib/libNet.so:
+
+/home/andrej/root_install/lib/libHist.so:
+
+/home/andrej/root_install/lib/libGraf.so:
+
+/home/andrej/root_install/lib/libGraf3d.so:
+
+/home/andrej/root_install/lib/libGpad.so:
+
+/home/andrej/root_install/lib/libROOTDataFrame.so:
+
+/home/andrej/root_install/lib/libTree.so:
+
+/home/andrej/root_install/lib/libTreePlayer.so:
+
+/home/andrej/root_install/lib/libRint.so:
+
+/home/andrej/root_install/lib/libPostscript.so:
+
+/home/andrej/root_install/lib/libMatrix.so:
+
+/home/andrej/root_install/lib/libPhysics.so:
+
+/home/andrej/root_install/lib/libMathCore.so:
+
+/home/andrej/root_install/lib/libThread.so:
+
+/home/andrej/root_install/lib/libMultiProc.so:
+
+/home/andrej/root_install/lib/libROOTVecOps.so:
+
+/home/andrej/libtorch/lib/libtorch_cpu.so:
+
+/home/andrej/libtorch/lib/libc10.so:
+
+/home/andrej/libtorch/lib/libtorch.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libstdc++.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libm.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libm.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libm.so:
+
+/lib64/libm.so.6:
+
+/lib64/libmvec.so.1:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so:
+
+/lib64/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libpthread.a:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libc.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libc.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libc.so:
+
+/lib64/libc.so.6:
+
+/usr/lib64/libc_nonshared.a:
+
+/lib64/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so:
+
+/lib64/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/crtend.o:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/crtn.o:
+
+/usr/lib64/libdl.so.2:
+
+/usr/lib64/libpthread.so.0:
+
+/lib64/ld-linux-x86-64.so.2:
+
+/home/andrej/miniconda3/lib/libpcre2-8.so.0:
+
+/home/andrej/miniconda3/lib/libz.so.1:
+
+/home/andrej/miniconda3/lib/liblzma.so.5:
+
+/home/andrej/miniconda3/lib/libzstd.so.1:
+
+/home/andrej/root_install/lib/libtbb.so.12:
+
+/home/andrej/miniconda3/lib/libssl.so.3:
+
+/home/andrej/miniconda3/lib/libcrypto.so.3:
+
+/usr/lib64/libfreetype.so.6:
+
+/home/andrej/root_install/lib/libROOTNTuple.so:
+
+/home/andrej/root_install/lib/libvdt.so:
+
+/home/andrej/root_install/lib/libGenVector.so:
+
+/usr/lib64/librt.so.1:
+
+/home/andrej/libtorch/lib/libgomp-98b21ff3.so.1:
+
+/usr/lib64/libbz2.so.1:
+
+/usr/lib64/libpng16.so.16:
+
+/usr/lib64/libharfbuzz.so.0:
+
+/usr/lib64/libbrotlidec.so.1:
+
+/usr/lib64/libglib-2.0.so.0:
+
+/usr/lib64/libgraphite2.so.3:
+
+/usr/lib64/libbrotlicommon.so.1:
